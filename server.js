bc0ad66a0990a3498017c5d4594d8749310b0311
jamesboblak <james@skepticalrecords.com>
2021-07-22 23:21:36 -0700

    added middleware

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const path = require('path');
 const PORT = 3001;
 const app = express();
 
-
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
   // Report that the app is listening to the Terminal
   app.listen(PORT, () =>
