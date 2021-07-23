dd561c6c696a750f6c3ae43b1fccf26d2ec79389
jamesboblak <james@skepticalrecords.com>
2021-07-23 00:58:31 -0700

    writing to db.json file

--- server.js
+++ server.js
@@ -32,6 +32,57 @@ app.get('/api/notes', (req, res) => {
   return;
 });
 
+// POST note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add a new note
+        parsedNotes.push(newNote);
+
+        // Write updated notess back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated notes!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
+
 
   // Report that the app is listening to the Terminal
   app.listen(PORT, () =>
