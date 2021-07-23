15b2b8e79d05214ab75d968bf0810f1625ad70c6
jamesboblak <james@skepticalrecords.com>
2021-07-22 23:31:16 -0700

    GETS to index.html and notes.html working in Insomnia

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// Routes
+// GET index.html page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
   // Report that the app is listening to the Terminal
   app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
